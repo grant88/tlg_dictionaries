25297552627ebea03b37109691f95c5f65d3efd9
grantovsky <uhfxtd@gmail.com>
2024-06-23 18:05:17 +0300

    init full functions

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import logging
 from db import DB
+from typing import Dict
+import polars as pl
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
@@ -10,37 +12,38 @@ from telegram.ext import (
     ContextTypes,
     ConversationHandler,
     MessageHandler,
-    filters
+    filters,
 )
 
-# Enable logging
+from telegram.constants import ParseMode
+
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 
-# Stages
-DICT_CHOICES_STAGE, BANK_STAGE, MCC_STAGE, END_STAGE = range(4)
-# Callback data
-DICT_CHOICES, BANK_CHOICES, MCC_CHOICES, BY_BIK, BY_MCC, READ_BIC, READ_MCC, END = range(8)
+DICT_CHOICES_STAGE, BANK_STAGE, MCC_STAGE, READ_STAGE, END_STAGE = range(5)
+(
+    DICT_CHOICES,
+    BANK_CHOICES,
+    MCC_CHOICES,
+    BANK_BY_BIK,
+    BANK_BY_NAME,
+    MCC_BY_CODE,
+    MCC_BY_DESCR,
+    END,
+) = range(8)
 
-TOKEN = os.environ.get('BANKS_LIST_TOKEN', 'Not Set')
-print(TOKEN)
+TOKEN = os.environ.get("BANKS_LIST_TOKEN", "TOKEN Not Set")
 
 db = DB()
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Send message on `/start`."""
-    # Get user that sent /start and log his name
     user = update.message.from_user
     logger.info("User %s started the conversation.", user.first_name)
-    # Build InlineKeyboard where each button has a displayed text
-    # and a string as callback_data
-    # The keyboard is a list of button rows, where each row is in turn
-    # a list (hence `[[...]]`).
     keyboard = [
         [
             InlineKeyboardButton("Инфо по банкам", callback_data=str(BANK_CHOICES)),
@@ -48,17 +51,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    # Send message with text and appended InlineKeyboard
-    await update.message.reply_text("Сервис, который позволяет получать информацию по разным справочникам. \nВыбери, что нужно", reply_markup=reply_markup)
-    # Tell ConversationHandler that we're in state `FIRST` now
+    text = """
+        Сервис, который позволяет получать информацию по разным справочникам.
+        Выбери справочник, который нужен
+    """
+    await update.message.reply_text(text=text, reply_markup=reply_markup)
     return DICT_CHOICES_STAGE
 
+
 async def start_over(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Prompt same text & keyboard as `start` does but not as new message"""
-    # Get CallbackQuery from Update
     query = update.callback_query
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     await query.answer()
     keyboard = [
         [
@@ -67,19 +69,17 @@ async def start_over(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    # Instead of sending a new message, edit the message that
-    # originated the CallbackQuery. This gives the feeling of an
-    # interactive menu.
-    await query.edit_message_text(text="Сервис, который позволяет получать информацию по разным справочникам. \nВыбери, что нужно", reply_markup=reply_markup)
+    text = """
+        Сервис, который позволяет получать информацию по разным справочникам.
+        Выбери справочник, который нужен
+    """
+    await query.edit_message_text(text=text, reply_markup=reply_markup)
     return DICT_CHOICES_STAGE
 
 
 async def dict_choices(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Prompt same text & keyboard as `start` does but not as new message"""
-    # Get CallbackQuery from Update
+    logger.info(f"{update=}\n{context=}")
     query = update.callback_query
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     await query.answer()
     keyboard = [
         [
@@ -88,7 +88,8 @@ async def dict_choices(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(text="Выбери, по какому справочнику искать", reply_markup=reply_markup)
+    text = f"Выбери, по какому справочнику искать"
+    await query.edit_message_text(text=text, reply_markup=reply_markup)
     return DICT_CHOICES_STAGE
 
 
@@ -97,14 +98,15 @@ async def bank_choices(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
     await query.answer()
     keyboard = [
         [
-            InlineKeyboardButton("Поиск по БИКу банка", callback_data=str(BY_BIK)),
-            #InlineKeyboardButton("Поиск по наименованию банка", callback_data=str(BY_BANK_NAME)),
+            InlineKeyboardButton("Поиск банка по БИКу", callback_data=str(BANK_BY_BIK)),
+            InlineKeyboardButton(
+                "Поиск банка по наименованию", callback_data=str(BANK_BY_NAME)
+            ),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(
-        text="Выбери критерий, по которому будешь искать банк", reply_markup=reply_markup
-    )
+    text = f"Выбери критерий, по которому будешь искать банк"
+    await query.edit_message_text(text=text, reply_markup=reply_markup)
     return BANK_STAGE
 
 
@@ -113,84 +115,149 @@ async def mcc_choices(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int
     await query.answer()
     keyboard = [
         [
-            InlineKeyboardButton("Поиск по MCC", callback_data=str(BY_MCC)),
-            #InlineKeyboardButton("Поиск по наименованию MCC", callback_data=str(BY_MCC_NAME)),
+            InlineKeyboardButton("Поиск по MCC", callback_data=str(MCC_BY_CODE)),
+            InlineKeyboardButton(
+                "Поиск по наименованию MCC", callback_data=str(MCC_BY_DESCR)
+            ),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(
-        text="Выбери критерий, по которому будешь искать MCC", reply_markup=reply_markup
-    )
+    text = "Выбери критерий, по которому будешь искать MCC"
+    await query.edit_message_text(text=text, reply_markup=reply_markup)
     return MCC_STAGE
 
 
-async def by_bik(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def bank_by_bik(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data["previous_step"] = "bank_by_bik"
     query = update.callback_query
     await query.answer()
     keyboard = [
         [
-            InlineKeyboardButton("Хочу искать банки по другому критерию", callback_data=str(BANK_CHOICES)),
+            InlineKeyboardButton(
+                "Искать банки по другому критерию", callback_data=str(BANK_CHOICES)
+            ),
         ],
         [
             InlineKeyboardButton("Главное меню", callback_data=str(DICT_CHOICES)),
-         ]
+        ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await query.edit_message_text(
-        text="Вводи БИК банка", reply_markup=reply_markup
+        text="Вводи БИК банка",
+        reply_markup=reply_markup,
     )
-    return BANK_STAGE
+    return READ_STAGE
 
-async def read_bik(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    bik = update.message.text
-    print(f"{bik=}")
-    keyboard = [
-        [
-            InlineKeyboardButton("Главное меню", callback_data=str(DICT_CHOICES)),
-         ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    #data = db.select(f"select * from public.banks where bic = {bik}")
-    #await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-    await update.message.reply_text(f"Данные по БИКу {bik}", reply_markup=reply_markup)
-    # Transfer to conversation state `SECOND`
-    return END_STAGE
 
-async def by_mcc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def mcc_by_code(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data["previous_step"] = "mcc_by_code"
     query = update.callback_query
     await query.answer()
     keyboard = [
         [
-            InlineKeyboardButton("Хочу искать MCC по другому критерию", callback_data=str(MCC_CHOICES)),
+            InlineKeyboardButton(
+                "Искать MCC по другому критерию", callback_data=str(MCC_CHOICES)
+            ),
         ],
         [
             InlineKeyboardButton("Главное меню", callback_data=str(DICT_CHOICES)),
-         ]
+        ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(
-        text="Вводи MCC", reply_markup=reply_markup
-    )
-    return MCC_STAGE
+    await query.edit_message_text(text="Вводи MCC", reply_markup=reply_markup)
+    return READ_STAGE
 
-async def read_mcc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    mcc = update.message.text
-    print(f"{mcc=}")
+
+async def bank_by_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data["previous_step"] = "bank_by_name"
+    query = update.callback_query
+    await query.answer()
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "Искать банки по другому критерию", callback_data=str(BANK_CHOICES)
+            ),
+        ],
+        [
+            InlineKeyboardButton("Главное меню", callback_data=str(DICT_CHOICES)),
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    text = "Вводи наименование банка"
+    await query.edit_message_text(text=text, reply_markup=reply_markup)
+    return READ_STAGE
+
+
+async def mcc_by_descr(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data["previous_step"] = "mcc_by_descr"
+    query = update.callback_query
+    await query.answer()
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "Искать MCC по другому критерию", callback_data=str(MCC_CHOICES)
+            ),
+        ],
+        [
+            InlineKeyboardButton("Главное меню", callback_data=str(DICT_CHOICES)),
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    text = "Вводи наименование кода ТСП"
+    await query.edit_message_text(text=text, reply_markup=reply_markup)
+    return READ_STAGE
+
+
+def reformat(data: pl.DataFrame) -> str:
+    if data.is_empty():
+        return "Ничего нашлось ("
+
+    line_len = 30
+    reformatted = "#" * line_len + "\n"
+
+    for row in data.iter_rows():
+        row_dict = dict(zip(data.columns, row))
+        row_line = (
+            "\n".join(
+                [f"<b>{k.upper()}</b>: <i>{v}</i>\n" for k, v in row_dict.items()]
+            )
+            + "\n"
+            + "#" * line_len
+            + "\n"
+        )
+        reformatted += row_line
+    logger.info(f"{reformatted=}")
+    return reformatted
+
+
+async def read_user_input_text(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    previous_step = context.user_data["previous_step"]
+    user_input_text = update.message.text
     keyboard = [
         [
             InlineKeyboardButton("Главное меню", callback_data=str(DICT_CHOICES)),
-         ]
+        ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    #data = db.select(f"select * from public.banks where bic = {bik}")
-    await update.message.reply_text(f"Данные по MCC {mcc}", reply_markup=reply_markup)
+    sqls = {
+        "bank_by_bik": f"select short_name, bik, date_started from public.banks where bik = {user_input_text}",
+        "mcc_by_code": f"select code, descr from public.mcc where code = {user_input_text}",
+        "bank_by_name": f"select short_name, bik, date_started from public.banks where short_name ilike '%{user_input_text}%'",
+        "mcc_by_descr": f"select code, descr from public.mcc where descr ilike '%{user_input_text}%'",
+    }
+    data = db.select(sqls[previous_step])
+    reformatted_data = reformat(data)
+    await update.message.reply_text(
+        text=reformatted_data,
+        parse_mode=ParseMode.HTML,
+        reply_markup=reply_markup,
+    )
     return END_STAGE
 
 
 async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Returns `ConversationHandler.END`, which tells the
-    ConversationHandler that the conversation is over.
-    """
     query = update.callback_query
     await query.answer()
     await query.edit_message_text(text="See you next time!")
@@ -198,30 +265,34 @@ async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
 
 def main() -> None:
-    """Run the bot."""
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token(TOKEN).build()
 
-    # Setup conversation handler with the states FIRST and SECOND
-    # Use the pattern parameter to pass CallbackQueries with specific
-    # data pattern to the corresponding handlers.
-    # ^ means "start of line/string"
-    # $ means "end of line/string"
-    # So ^ABC$ will only allow 'ABC'
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
             DICT_CHOICES_STAGE: [
-                CallbackQueryHandler(bank_choices, pattern="^" + str(BANK_CHOICES) + "$"),
+                CallbackQueryHandler(
+                    bank_choices, pattern="^" + str(BANK_CHOICES) + "$"
+                ),
                 CallbackQueryHandler(mcc_choices, pattern="^" + str(MCC_CHOICES) + "$"),
             ],
             BANK_STAGE: [
-                CallbackQueryHandler(by_bik, pattern="^" + str(BY_BIK) + "$"),
-                MessageHandler(filters=filters.TEXT & ~filters.COMMAND, callback=read_bik),
+                CallbackQueryHandler(bank_by_bik, pattern="^" + str(BANK_BY_BIK) + "$"),
+                CallbackQueryHandler(
+                    bank_by_name, pattern="^" + str(BANK_BY_NAME) + "$"
+                ),
             ],
             MCC_STAGE: [
-                CallbackQueryHandler(by_mcc, pattern="^" + str(BY_MCC) + "$"),  
-                MessageHandler(filters=filters.TEXT & ~filters.COMMAND, callback=read_mcc),
+                CallbackQueryHandler(mcc_by_code, pattern="^" + str(MCC_BY_CODE) + "$"),
+                CallbackQueryHandler(
+                    mcc_by_descr, pattern="^" + str(MCC_BY_DESCR) + "$"
+                ),
+            ],
+            READ_STAGE: [
+                MessageHandler(
+                    filters=filters.TEXT & ~filters.COMMAND,
+                    callback=read_user_input_text,
+                ),
             ],
             END_STAGE: [
                 CallbackQueryHandler(start_over, pattern="^" + str(DICT_CHOICES) + "$"),
@@ -231,11 +302,7 @@ def main() -> None:
         fallbacks=[CommandHandler("start", start)],
     )
 
-    # Add ConversationHandler to application that will be used for handling updates
     application.add_handler(conv_handler)
-#    application.add_handler(bik_handler)
-
-    # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
--- import.py
+++ import.py
@@ -4,14 +4,25 @@ import pandas as pd
 import polars as pl
 from db import DB
 
-url = os.environ.get("BANKS_URL", None)
-resp = requests.get(url)
-df = pd.read_xml(resp.content, encoding='windows-1251', parser='etree')
 
-print(len(df))
+def banks() -> pl.DataFrame:
+    url: str | None = os.environ.get("BANKS_URL", None)
+    if url:
+        resp = requests.get(url)
+        df = pd.read_xml(resp.content, encoding="windows-1251", parser="etree")
+        df.columns = ["id", "date_started", "short_name", "bik"]
+    return pl.from_pandas(df)
 
-pl_df = pl.from_pandas(df)
+
+def mcc() -> pl.DataFrame:
+    df = pd.read_excel("downloads/mcc.xlsx")
+    df.columns = ["code", "descr"]
+    return pl.from_pandas(df)
+
 
 db = DB()
 
-db.insert(table='banks', df=pl_df)
+banks_df = banks()
+mcc_df = mcc()
+db.insert(table="mcc", df=banks_df)
+db.insert(table="mcc", df=mcc_df)
